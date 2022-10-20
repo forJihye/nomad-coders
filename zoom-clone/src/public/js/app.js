d1328314c1991aa8936cae27b1dca8aa28c14fa7
byjihye <byjihye@hashsnap.net>
2022-10-20 18:42:05 +0900

    #1. zoom clone

--- zoom-clone/src/public/js/app.js
+++ zoom-clone/src/public/js/app.js
@@ -1,1 +1,27 @@
+const messageList = document.querySelector("ul");
+const messageFrom = document.querySelector("form");
+
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener('open', () => {
+  console.log('Connected to Server  ✅')
+})
+
+socket.addEventListener('message', (message) => {
+  console.log('New Message: ', message.data);
+})
+
+socket.addEventListener('close', () => {
+  console.log('Disconnected from Server ❌')
+})
+
+// setTimeout(() => {
+//   socket.send("hello from the Browser!")
+// }, 10000);
+const handlerSubmit = (ev) => {
+  ev.preventDefault();
+  const input = messageFrom.querySelector('input');
+  socket.send(input.value);
+  input.value = "";
+}
+messageFrom.addEventListener('submit', handlerSubmit)
--- zoom-clone/src/server.js
+++ zoom-clone/src/server.js
@@ -13,12 +13,18 @@ app.get('/', (req, res) => res.render('home'));
 // app.listen(3000, () => console.log('http://localhost:3000'));
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
-const handlerConnection = (socket) => {
-  // socket : 연결된 브라우저
-  console.log(socket)
-}
-wss.on("connection", handlerConnection);
+const sockets = new Set();
+
+const wss = new WebSocket.Server({ server });
+wss.on("connection", (socket) => { // socket : 연결된 브라우저
+  sockets.add(socket);
+  console.log('Connected to Browser ✅');
+  socket.on("close", () => console.log('Disconnected from the Browser ❌'))
+  socket.on('message', message => {
+    socket.send(message.toString());
+  })
+  socket.send('hello');
+});
 
 server.listen(3000, () => console.log('http://localhost:3000, ws://localhost:3000'));
